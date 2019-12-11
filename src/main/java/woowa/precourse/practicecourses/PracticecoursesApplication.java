12a27aa4614480b15f075644c8acf1c5f051b680
KIMSIYOUNG <tldud2404@gmail.com>
2019-12-12 00:00:07 +0900

    first init

---
+++ src/main/java/woowa/precourse/practicecourses/PracticecoursesApplication.java
@@ -0,0 +1,13 @@
+package woowa.precourse.practicecourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticecoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticecoursesApplication.class, args);
+    }
+
+}
